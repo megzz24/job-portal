392e5d5d40da255466477538dff3bea696772dc6
megzz24 <meghnavs24@gmail.com>
2026-01-04 12:05:59 +0530

    Fix Cloudinary raw file public access

--- backend/backend/storage.py
+++ backend/backend/storage.py
@@ -1,4 +1,12 @@
-from cloudinary_storage.storage import RawMediaCloudinaryStorage
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
-class RawCloudinaryStorage(RawMediaCloudinaryStorage):
+class RawCloudinaryStorage(MediaCloudinaryStorage):
     resource_type = "raw"
+
+    def get_upload_options(self, name, content=None):
+        options = super().get_upload_options(name, content)
+        options.update({
+            "resource_type": "raw",
+            "access_mode": "public",   # 👈 THIS is mandatory
+        })
+        return options
